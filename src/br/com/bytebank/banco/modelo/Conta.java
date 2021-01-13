5f2cf14649fd4c394f4ea795a5ebfa615d1c9cb9
DouglasBasilio <basilio.douglas@yahoo.com.br>
2021-01-12 21:02:24 -0300

    Comparator, compare, sort and implements Comparable

--- src/br/com/bytebank/banco/modelo/Conta.java
+++ src/br/com/bytebank/banco/modelo/Conta.java
@@ -9,7 +9,7 @@ package br.com.bytebank.banco.modelo;
 
 // A classe ainda existe, mas não pode mais criar um objeto dessa classe
 
-public abstract class Conta {
+public abstract class Conta extends Object implements Comparable<Conta> {
 	protected double saldo;
 	private int agencia;
 	private int numero;
@@ -117,7 +117,13 @@ public abstract class Conta {
 	}
 
 	@Override
+	public int compareTo(Conta outra) {
+		
+		return Double.compare(this.saldo, outra.saldo);
+	}
+	
+	@Override
 	public String toString() {
-		return "Número: " + this.numero + " - Agência: " + this.agencia;
+		return "Número: " + this.numero + " - Agência: " + this.agencia + " - Saldo: " + this.saldo;
 	}
 }
